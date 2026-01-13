f369bf11b299690197c413c813988f935bb4a950
rishu8925 <rishu.upadhyay_cs23@gla.ac.in>
2026-01-13 15:14:03 +0530

    add custom file

---
+++ Systemlogger.js
@@ -0,0 +1,20 @@
+const os = require('os');
+const fs = require('fs');
+
+setInterval(() => {
+  const systemInfo = `
+Time: ${new Date().toLocaleString()}
+Platform: ${os.platform()}
+CPU Cores: ${os.cpus().length}
+Free Memory: ${os.freemem()}
+Total Memory: ${os.totalmem()}
+---------------------------
+`;
+
+  fs.appendFile('systemInfo.txt', systemInfo, (err) => {
+    if (err) {
+      console.error('Error writing system info');
+    }
+  });
+
+}, 5000);
